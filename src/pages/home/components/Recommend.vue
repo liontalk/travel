643cf5d0b5ac136570ed77db9e3834fabcf14670
liontalk@163.com <Liontalk@1234>
2019-08-03 17:30:42 +0800

    首页推荐显示

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item">
+        <div class="item-image-wrapper">
+
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: "Recommend",
+    props: []
+  }
+
+</script>
+<style scoped lang="stylus">
+
+  .title
+    margin-top .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent .2rem
+
+</style>
