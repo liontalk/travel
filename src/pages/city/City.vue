54f917e1680903125b41188797470541f256e772
liontalk@163.com <liontalk@163.com>
2019-08-25 17:05:53 +0800

    完成城市列表布局

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,13 +2,14 @@
   <div>
     <CityHeader></CityHeader>
     <city-search></city-search>
-    <CityList></CityList>
+    <CityList :city="cities" :hotCities="hotCities"></CityList>
     <CityAlphabet></CityAlphabet>
   </div>
 </template>
 
 
 <script>
+  import axios from 'axios'
   import CityHeader from './components/CityHeader'
   import CitySearch from './components/CitySearch'
   import CityList from './components/CityList'
@@ -20,7 +21,32 @@
       CityHeader: CityHeader,
       CitySearch: CitySearch,
       CityList: CityList,
-      CityAlphabet : CityAlphabet
+      CityAlphabet: CityAlphabet
+    },
+    data() {
+      return {
+        cities: {},
+        hotCities: []
+      }
+    },
+    methods: {
+      getCityInfo() {
+        axios.get('http://192.168.1.12:8888/api/city/data').then(this.handleGetCityInfoSuccess);
+      },
+      handleGetCityInfoSuccess(res) {
+        res = res.data;
+        if (res.code == 200 && res.data != null) {
+          let result = res;
+          let data = JSON.parse(result.data);
+          this.hotCities = data.data.hotCities;
+          this.cities = data.data.cities;
+          console.log(this.cities);
+          console.log( this.hotCities);
+        }
+      }
+    },
+    mounted() {
+      this.getCityInfo();
     }
   }
 
