7c9f0134221be629237d7efa41e97cb6c30a89ee
liontalk@163.com <liontalk@163.com>
2019-08-25 13:46:59 +0800

    增加城市列表

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,21 +2,24 @@
   <div>
     <CityHeader></CityHeader>
     <city-search></city-search>
+    <CityList></CityList>
   </div>
 </template>
 
 
 <script>
-    import CityHeader from './components/CityHeader'
-    import CitySearch from './components/CitySearch'
+  import CityHeader from './components/CityHeader'
+  import CitySearch from './components/CitySearch'
+  import CityList from './components/CityList'
 
-    export default {
-        name: 'city',
-        components: {
-            CityHeader: CityHeader,
-            CitySearch: CitySearch
-        }
+  export default {
+    name: 'city',
+    components: {
+      CityHeader: CityHeader,
+      CitySearch: CitySearch,
+      CityList: CityList
     }
+  }
 
 </script>
 <style scoped>
---
+++ src/pages/city/components/CityList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>list</p>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: "CityList",
+    props: []
+  }
+
+</script>
+<style scoped  lang="stylus">
+  @import "~@/assets/css/iconfont/varibles.styl";
+</style>
