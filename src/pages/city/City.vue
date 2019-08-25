5611f8d2ba336af05f520cd8d78fbde87884fc17
liontalk@163.com <liontalk@163.com>
2019-08-25 15:29:33 +0800

    完成城市列表布局

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,6 +3,7 @@
     <CityHeader></CityHeader>
     <city-search></city-search>
     <CityList></CityList>
+    <CityAlphabet></CityAlphabet>
   </div>
 </template>
 
@@ -11,13 +12,15 @@
   import CityHeader from './components/CityHeader'
   import CitySearch from './components/CitySearch'
   import CityList from './components/CityList'
+  import CityAlphabet from './components/CityAlphabet'
 
   export default {
     name: 'city',
     components: {
       CityHeader: CityHeader,
       CitySearch: CitySearch,
-      CityList: CityList
+      CityList: CityList,
+      CityAlphabet : CityAlphabet
     }
   }
 
